b130ef4c4e75ac66480c1b364498e20761b9d125
tomo3110 <uotias64_mole@yahoo.co.jp>
2020-01-01 10:02:05 +0900

    add test case and add logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,36 @@
+use lazy_static::lazy_static;
+use std::env;
+
+mod errors;
+
+pub use ejdict_rs_core::{Dictionary, Word};
+pub use errors::{Error, ErrorKind, Result};
+
+lazy_static! {
+    static ref EJDICT_DISCIONARY: Dictionary = load_dictionary().unwrap();
+}
+
+fn load_dictionary() -> Result<Dictionary> {
+    let src = if cfg!(windows) {
+        include_str!(concat!(env!("OUT_DIR"), "\\ejdict.json"))
+    } else {
+        include_str!(concat!(env!("OUT_DIR"), "/ejdict.json"))
+    };
+    let dict = serde_json::from_str::<Dictionary>(src)?;
+    Ok(dict)
+}
+
+pub fn search(word: &str) -> Option<&Word> {
+    let ref dict: Dictionary = *EJDICT_DISCIONARY;
+    dict.search(word)
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::search;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn text_search() {
+        assert_ne!(search("Will"), None)
     }
 }
